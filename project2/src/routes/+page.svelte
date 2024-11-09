eb77c333e006d0937b0724b93942edee7969e58a
RhodCoding <joewee321@gmail.com>
2024-11-09 22:23:48 +0800

    Add project2 folder with project files

---
+++ project2/src/routes/+page.svelte
@@ -0,0 +1,46 @@
+<div class="menu">
+
+  <div class="menu-list">
+    <div class="menu-card">
+      <a href="/shop">
+       <img src="/assets/menu1.png" alt="menu1">
+      </a>
+    </div>
+
+    <div class="menu-card">
+      <a href="/shop">
+        <img src="/assets/menu2.png" alt="menu2">
+      </a>
+    </div>
+
+    <div class="menu-card">
+      <a href="/shop">
+        <img src="/assets/menu3.png" alt="menu3">
+      </a>
+    </div>
+  </div>
+     <div class="text">
+        <p>Check out our newest blends and bread</p>
+        <button>menu</button>
+     </div>
+</div>
+
+<div class="text2">
+   <h1>Brews & Buns</h1>
+   <p>Daily Dose of Deliciousness</p>
+</div>
+
+<div class="footer">
+   <img src="/assets/logo.png" alt="logo" class="logo">
+   <div class="social-media">
+      <a href="https://youtu.be/dQw4w9WgXcQ?si=88dyHexDiZOMwSSr">
+         <img src="/assets/twitter.png" alt="logo" class="twitter">
+      </a>
+      <a href="https://youtu.be/dQw4w9WgXcQ?si=88dyHexDiZOMwSSr">
+         <img src="/assets/instagram.png" alt="logo" class="insta">
+      </a>
+      <a href="https://youtu.be/dQw4w9WgXcQ?si=88dyHexDiZOMwSSr">
+         <img src="/assets/facebook.png" alt="logo" class="fb">
+      </a>
+   </div>
+</div>
