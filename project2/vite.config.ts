d413dab26166a1d6fe06767c70060aec1ce52dbe
RhodCoding <joewee321@gmail.com>
2024-11-09 22:37:09 +0800

    test 1

--- project2/vite.config.ts
+++ project2/vite.config.ts
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vitest/config';
-import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+  plugins: [svelte()],
+  base: '/project2/', // Base path for GitHub Pages
 });
