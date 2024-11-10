b9faf5699ef1aeaf9589f51e6275453dc1f93f51
RhodCoding <joewee321@gmail.com>
2024-11-10 22:47:09 +0800

    Footer Update Tailwindcss

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,46 +1,48 @@
-<div class="menu">
-
-  <div class="menu-list">
-    <div class="menu-card">
-      <a href="/shop">
-       <img src="/assets/menu1.png" alt="menu1">
-      </a>
-    </div>
-
-    <div class="menu-card">
-      <a href="/shop">
-        <img src="/assets/menu2.png" alt="menu2">
-      </a>
-    </div>
-
-    <div class="menu-card">
-      <a href="/shop">
-        <img src="/assets/menu3.png" alt="menu3">
-      </a>
-    </div>
-  </div>
-     <div class="text">
-        <p>Check out our newest blends and bread</p>
-        <button>menu</button>
+<div class="menu mt-6 bg-gray-400 bg-opacity-25 w-full h-[348px]">
+   <div class="menu-list relative w-full h-[70%] flex justify-around items-end">
+     <div class="menu-card relative w-[286px] h-[194px]">
+       <a href="/shop">
+         <img src="/assets/menu1.png" alt="menu1" class="w-full h-full object-cover">
+       </a>
      </div>
-</div>
-
-<div class="text2">
-   <h1>Brews & Buns</h1>
-   <p>Daily Dose of Deliciousness</p>
-</div>
-
-<div class="footer">
-   <img src="/assets/logo.png" alt="logo" class="logo">
-   <div class="social-media">
-      <a href="https://youtu.be/dQw4w9WgXcQ?si=88dyHexDiZOMwSSr">
-         <img src="/assets/twitter.png" alt="logo" class="twitter">
-      </a>
-      <a href="https://youtu.be/dQw4w9WgXcQ?si=88dyHexDiZOMwSSr">
-         <img src="/assets/instagram.png" alt="logo" class="insta">
-      </a>
-      <a href="https://youtu.be/dQw4w9WgXcQ?si=88dyHexDiZOMwSSr">
-         <img src="/assets/facebook.png" alt="logo" class="fb">
-      </a>
+ 
+     <div class="menu-card relative w-[286px] h-[194px]">
+       <a href="/shop">
+         <img src="/assets/menu2.png" alt="menu2" class="w-full h-full object-cover">
+       </a>
+     </div>
+ 
+     <div class="menu-card relative w-[286px] h-[194px]">
+       <a href="/shop">
+         <img src="/assets/menu3.png" alt="menu3" class="w-full h-full object-cover">
+       </a>
+     </div>
    </div>
-</div>
+ 
+   <div class="text relative w-full h-[30%] flex justify-center items-center gap-[100px] p-0 m-0">
+     <p class="text-xl mx-[100px] border-b-2 border-black">Check out our newest blends and bread</p>
+     <button class="bg-[#BA7A3A] w-[160px] h-[64px] rounded-xl text-lg font-bold uppercase">menu</button>
+   </div>
+ </div>
+ 
+ <div class="text2 h-[348px] flex flex-col items-center">
+   <h1 class="text-4xl font-bold mt-[50px]">Brews & Buns</h1>
+   <p class="text-xl">Daily Dose of Deliciousness</p>
+ </div>
+ 
+ <div class="footer bg-[#964B00] h-[92px] w-full flex justify-around items-center gap-[800px]">
+   <img src="/assets/logo.png" alt="logo" class="logo">
+   <div class="social-media flex justify-between items-center w-[150px] h-[36px]">
+     <a href="https://youtu.be/dQw4w9WgXcQ?si=88dyHexDiZOMwSSr">
+       <img src="/assets/twitter.png" alt="logo" class="w-[34px] h-[36px]">
+     </a>
+     <a href="https://youtu.be/dQw4w9WgXcQ?si=88dyHexDiZOMwSSr">
+       <img src="/assets/instagram.png" alt="logo" class="w-[34px] h-[36px]">
+     </a>
+     <a href="https://youtu.be/dQw4w9WgXcQ?si=88dyHexDiZOMwSSr">
+       <img src="/assets/facebook.png" alt="logo" class="w-[34px] h-[36px]">
+     </a>
+   </div>
+ </div>
+ 
+ 
