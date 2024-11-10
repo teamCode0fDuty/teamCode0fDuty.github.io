536082deac16fea795b25e124563e188c36c0cfd
RhodCoding <joewee321@gmail.com>
2024-11-10 14:23:40 +0800

    Add Visit Us Page Content

--- src/routes/shop/+page.svelte
+++ src/routes/shop/+page.svelte
@@ -1,1 +1,15 @@
-<p>shop</p>
+
+<div class="footer">
+    <img src="/assets/logo.png" alt="logo" class="logo">
+    <div class="social-media">
+       <a href="https://youtu.be/dQw4w9WgXcQ?si=88dyHexDiZOMwSSr">
+          <img src="/assets/twitter.png" alt="logo" class="twitter">
+       </a>
+       <a href="https://youtu.be/dQw4w9WgXcQ?si=88dyHexDiZOMwSSr">
+          <img src="/assets/instagram.png" alt="logo" class="insta">
+       </a>
+       <a href="https://youtu.be/dQw4w9WgXcQ?si=88dyHexDiZOMwSSr">
+          <img src="/assets/facebook.png" alt="logo" class="fb">
+       </a>
+    </div>
+ </div>
--- src/routes/visit-us/+page.svelte
+++ src/routes/visit-us/+page.svelte
@@ -1,1 +1,9 @@
-<p>visit us</p>
+<div class="location-chart">
+    <h1>Where to Find us</h1>
+    <div class="chart-img">
+        <img src="/assets/map.png" alt="">
+        <div class="tapi">
+            <p>Tapinac Oval Sports Complex, Donor St., East Tapinac, Olongapo, Philippines, 2200</p>
+        </div>
+    </div>
+</div>
