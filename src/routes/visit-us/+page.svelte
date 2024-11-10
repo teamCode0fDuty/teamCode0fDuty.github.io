bdfc9c4c1da764cbe623675700cbcc6ca224859a
RhodCoding <joewee321@gmail.com>
2024-11-10 22:33:53 +0800

    Change Visit Us Card to Column

--- src/routes/visit-us/+page.svelte
+++ src/routes/visit-us/+page.svelte
@@ -1,9 +1,11 @@
-<div class="location-chart w-full h-[422px] text-center flex flex-col items-center justify-center">
+<div class="location-chart w-full h-[422px] mt-64 text-center flex flex-col items-center justify-center">
     <h1 class="text-4xl mb-10 font-bold">Where to Find Us</h1>
-    <div class="chart-img flex justify-around items-center gap-20">
-        <img class="w-[492px] h-[260px]" src="/assets/map.png" alt="">
-        <div class="tapi w-[492px] h-[260px] bg-[rgba(100,98,98,0.5)] flex items-center justify-center p-12">
-            <p class="text-2xl text-left">Tapinac Oval Sports Complex, Donor St., East Tapinac, Olongapo, Philippines, 2200</p>
+    <div class="chart-img flex justify-around flex-col items-center gap-20">
+        <img class="w-[592px] h-[360px]" src="/assets/map.png" alt="">
+        <div class="tapi w-[592px] h-[360px] bg-[rgba(100,98,98,0.5)] flex items-center justify-center p-12">
+            <p class="text-2xl text-left">
+                Tapinac Oval Sports Complex, Donor St., East Tapinac, Olongapo, Philippines, 2200
+            </p>
         </div>
     </div>
 </div>
